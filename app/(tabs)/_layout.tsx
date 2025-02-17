2858e9345e0fd4aee8aa7b6ef5951176d05e3d78
MadyanArashy <madyan.arashy5@smk.belajar.id>
2025-02-17 15:34:09 +0700

    chore: create new tabs files

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,6 +4,7 @@ import { Platform } from 'react-native';
 
 import { HapticTab } from '@/components/HapticTab';
 import { IconSymbol } from '@/components/ui/IconSymbol';
+import { MaterialIcons, MaterialCommunityIcons, Octicons } from '@expo/vector-icons';
 import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
@@ -37,28 +38,28 @@ export default function TabLayout() {
         name="foryou"
         options={{
           title: 'For You',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          tabBarIcon: ({ color }) => <MaterialIcons size={28} name="wifi-tethering" color={color} />,
         }}
       />
       <Tabs.Screen
         name="live"
         options={{
           title: 'Live',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          tabBarIcon: ({ color }) => <MaterialCommunityIcons size={28} name="compass-outline" color={color} />,
         }}
       />
       <Tabs.Screen
         name="watchlist"
         options={{
           title: 'Watch List',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          tabBarIcon: ({ color }) => <Octicons size={28} name="stack" color={color} />,
         }}
       />
       <Tabs.Screen
         name="profile"
         options={{
           title: 'Profile',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          tabBarIcon: ({ color }) => <MaterialIcons size={28} name="person" color={color} />,
         }}
       />
     </Tabs>
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,36 +1,32 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { View, Text } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { ScrollView } from 'react-native'
+import { ThemedView } from '@/components/ThemedView'
+import { ThemedText } from '@/components/ThemedText'
+import { ThemedImage } from '@/components/ThemedImage'
+import { ThemedButton } from '@/components/ThemedButton'
+import { Ionicons } from '@expo/vector-icons'
+import tw from 'twrnc';
 
 
 export default function HomeScreen() {
   return (
-    <ThemedView>
-      <ThemedText>
-        Tab Home
-      </ThemedText>
+    <ThemedView style={tw`flex-1`}>
+      <SafeAreaView>
+        <ScrollView>
+          <ThemedView style={tw`px-4 flex-col h-full flex-1`}>
+            <View style={tw`flex-1 flex-row justify-between`}>
+              <ThemedImage width={100} height={50} sourceImage={require('@/assets/images/vidio-logo-long.png')}/>
+              <View style={tw`flex-row justify-center gap-4 items-center`}>
+                <Ionicons name='search' size={25}/>
+                <Ionicons name='notifications' size={25}/>
+                <Ionicons name='menu' size={25}/>
+              </View>
+            </View>
+          </ThemedView>
+        </ScrollView>
+      </SafeAreaView>
     </ThemedView>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
